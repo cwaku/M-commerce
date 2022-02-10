f81ab790a29c1834d4cb5dae9d0d8feb8063942f
vic778 <victoiremmanuelbarh@gmail.com>
2022-02-10 17:02:10 +0200

    Create hover for Info and give it the opacity

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Announcement from '../components/Announcement';
+import Categories from '../components/Categories';
+import Navbar from '../components/Navbar';
+import Products from '../components/Products';
+import Slider from '../components/Slider';
+
+const Home = () => (
+  <div>
+    <Announcement />
+    <Navbar />
+    <Slider />
+    <Categories />
+    <Products />
+  </div>
+);
+
+export default Home;
